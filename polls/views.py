5251724b7c878de796f1c1804298176c3a799485
Pichapa <pichapa.t@ku.th>
2021-09-03 20:41:15 +0700

    fix show result after vote

--- polls/views.py
+++ polls/views.py
@@ -17,9 +17,7 @@ class IndexView(generic.ListView):
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(
-           pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]    
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]    
 
 class DetailView(generic.DetailView):
     model = Question
@@ -31,13 +29,9 @@ class DetailView(generic.DetailView):
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
-    moedel = Question
+    model = Question
     template_name = 'polls/results.html'
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+    
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
