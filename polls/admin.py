6bed9043a64126b43bb7c5314b6f9195e6b816e4
Pichapa <pichapa.t@ku.th>
2021-09-08 11:22:23 +0700

    rename result.html to results.htme

--- polls/admin.py
+++ polls/admin.py
@@ -15,8 +15,8 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date', 'end_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently', 'is_published', 'can_vote')
-    list_filter = ['pub_date']
+    list_display = ('question_text', 'pub_date', 'was_published_recently', 'is_published', 'can_vote', 'end_date')
+    list_filter = ['pub_date', 'end_date']
     search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
